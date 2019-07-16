0b6baf56c19460d076598e0d1ee4f614b2c5d157
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-16 19:13:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  # Your code here
+  array.map { |word|
+    "#{word.capitalize}!"
+  }
 end
 
 def long_planeteer_calls(array)
